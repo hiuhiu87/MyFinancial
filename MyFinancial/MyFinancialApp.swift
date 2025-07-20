610b31cb7780979815b3c060b6f70fe4b3f61b57
hiuhiu87 <minhhieu8723@gmail.com>
2025-07-20 12:52:29 +0700

    Initial Commit

---
+++ MyFinancial/MyFinancialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinancialApp.swift
+//  MyFinancial
+//
+//  Created by NMH on 20/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFinancialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
